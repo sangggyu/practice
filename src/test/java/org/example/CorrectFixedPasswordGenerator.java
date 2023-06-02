a47fed3ba9f543d3d4bf883aeaa402e3ef452eb2
sangggyu <124053404+sangggyu@users.noreply.github.com>
2023-06-02 11:10:15 +0900

    Update CorrectFixedPasswordGenerator.java

--- src/test/java/org/example/CorrectFixedPasswordGenerator.java
+++ src/test/java/org/example/CorrectFixedPasswordGenerator.java
@@ -4,5 +4,7 @@ public class CorrectFixedPasswordGenerator implements PasswordGenerator{
     @Override
     public String generatePassword() {
         return "abcdefgh"; // 8글자
+        
+        
     }
 }
